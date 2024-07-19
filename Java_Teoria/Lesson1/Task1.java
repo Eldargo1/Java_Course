20159cf9443be994ff8d1a745ebe0cc648668fcc
eldar <kastoom204@gmail.com>
2024-07-20 01:59:59 +0500

    Lesson1

---
+++ Java_Teoria/Lesson1/Task1.java
@@ -0,0 +1,11 @@
+package Java_Teoria.Lesson1;
+
+/**
+ * Task1
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        String s = "24";
+        System.out.println(s);
+    }
+}
